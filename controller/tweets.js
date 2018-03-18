610ce308b30b6ca537e36db7f3437ce0d56a435b
Ankit <ankitsocial16@gmail.com>
2018-03-18 08:13:06 +0530

    added tweets array

--- controller/tweets.js
+++ controller/tweets.js
@@ -1,15 +1,28 @@
 module.exports = function (sqldb, callback) {
     let db = sqldb(); // starting database connection
+    let data = {tweets:[]};
     let sql = `SELECT * FROM latest_tweets`; // All tweets
     db.serialize(function() {
-        db.all(sql , (err, row) => {
+        db.each(sql , (err, row) => {
             if (err) {
                 return console.error(err.message);
             }
-            return row
-                ? callback(row)
-                : console.log(`No Data found`);
-        })
-    })
+            else{
+                let arr = row.url.split('/');
+                let id = arr[arr.length - 1].toString();
+                data.tweets.push({
+                    coin_name       :row.coin_name,
+                    coin_symbol:    row.coin_symbol,
+                    coin_handle: row.coin_handle,
+                    tweet:row.tweet,
+                    url:row.url,
+                    date:row.date,
+                    keyword:row.keyword,
+                    id:id
+                })
+            }
+        });
+        db.all(sql, (err, row) => {callback(data)})
+    });
     db.close() // closing database connection
 };
